88d54bd612a6fce183d569b6878eac3ce6498f4c
padoski <43954426+padoski@users.noreply.github.com>
2019-05-27 21:56:41 +0200

    Stworzylismy projekt
    Lol Lukasz

---
+++ SFML ProjectGra1/SFML ProjectGra1/Main.cpp
@@ -0,0 +1,31 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
+
+using namespace sf;
+
+int main()
+{
+	RenderWindow window(VideoMode(800, 600), "Example 03!", Style::Default);
+	window.setFramerateLimit(60);
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+		//Update
+
+		//Draw
+		window.clear();
+
+		//Draw everything
+		window.display();
+	}
+
+	return 0;
+}
